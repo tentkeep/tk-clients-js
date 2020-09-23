de0a9dcb0957d6903a8c9d632f41125e956433a3
Justin Wilkey <jwilkey@tentkeep.com>
2020-09-23 01:17:28 -0400

    Add musickit.artistSummary

--- src/clients/musickit.js
+++ src/clients/musickit.js
@@ -2,12 +2,46 @@ const { api } = require('../api')
 const host = 'https://api.music.apple.com'
 const jwt = require('jsonwebtoken')
 
+const getArtistAlbums = artistId => music(`/v1/catalog/us/artists/${artistId}/albums?include=tracks`)
+
 module.exports = {
-  searchArtists: term => music(`/v1/catalog/us/search?term=${term}&limit=25&types=artists&include=albums`),
+  searchArtists: term => music(`/v1/catalog/us/search?term=${term}&limit=25&types=artists`),
   getArtist: artistId => music(`/v1/catalog/us/artists/${artistId}?include=albums,songs`),
-  getArtistAlbums: artistId => music(`/v1/catalog/us/artists/${artistId}/albums?include=tracks`),
+  getArtistAlbums,
   getAlbum: albumId => music(`/v1/catalog/us/albums/${albumId}`),
-  getAlbums: albumIds => music(`/v1/catalog/us/albums?ids=${albumIds.join(',')}`)
+  getAlbums: albumIds => music(`/v1/catalog/us/albums?ids=${albumIds.join(',')}`),
+  artistSummary: async artistId => {
+    const artistAlbums = await getArtistAlbums(artistId)
+    const albums = artistAlbums.data.map(a => ({
+      id: a.id,
+      title: a.attributes.name,
+      image: a.attributes.artwork.url.replace(/{w}|{h}/g, 600),
+      releaseDate: a.attributes.releaseDate,
+      recordLabel: a.attributes.recordLabel,
+      copyright: a.attributes.copyright,
+      isSingle: a.attributes.isSingle,
+      tracks: (((a.relationships || {}).tracks || {}).data || []).map(t => ({
+        id: t.id,
+        title: t.attributes.name,
+        preview: t.attributes.previews[0].url,
+        trackNumber: t.attributes.trackNumber,
+        duration: t.attributes.durationInMillis,
+        isrc: t.attributes.isrc,
+        services: {
+          apple: { id: t.id, url: t.attributes.url, kind: t.attributes.playParams.kind }
+        }
+      })),
+      services: {
+        apple: { id: a.id, url: a.attributes.url }
+      }
+    }))
+    return {
+      id: artistId,
+      title: artistAlbums.data[0].attributes.artistName,
+      image: artistAlbums.data[0].attributes.artwork.url.replace(/{w}|{h}/g, 600),
+      albums
+    }
+  }
 }
 
 const music = (path) => {
